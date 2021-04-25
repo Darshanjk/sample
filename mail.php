668a8a62065ce74ea8fac047eb710ebe57903342
Darshanjk <75562276+Darshanjk@users.noreply.github.com>
2021-04-25 17:13:55 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,7 +6,7 @@ $message= $_POST['message'];
 $to = "darshanvj21@gmail.com";
 $subject = "Mail From jk";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: https://github.com/Darshanjk" . "\r\n" .
+$headers = "From: noreply@codeconia.com " .\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
